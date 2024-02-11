b003447ea29e5e92ee5d297ac68648c72fe08318
Churro <charanvenkatchandran@gmail.com>
2024-02-11 01:28:09 -0600

    Added other scripts some are old though

--- removeTypeGameSound.js
+++ removeTypeGameSound.js
@@ -1,14 +1,12 @@
 // ==UserScript==
 // @name         removeTypeGameSound
 // @namespace    http://tampermonkey.net/
-// @version      2023-12-30
+// @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.typing.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=typing.com
 // @grant        none
-// @downloadURL  https://github.com/ChurroC/TamperMonkey/raw/main/script.user.js
-// @updateURL    https://github.com/ChurroC/TamperMonkey/raw/main/script.user.js
 // ==/UserScript==
 
 (async function () {
