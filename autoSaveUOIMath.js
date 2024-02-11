c9b7cda8f9dc14dc1f989d639903e72813b73683
Churro <charanvenkatchandran@gmail.com>
2024-02-11 01:22:10 -0600

    finally got this to work

--- autoSaveUOIMath.js
+++ autoSaveUOIMath.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         autoSave
+// @name         autoSaveUOIMath
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -26,22 +26,16 @@
     const amountOfTabs = 1;
 
     const observer = new MutationObserver(async () => {
-        console.log(document.querySelectorAll('.x-box-target')[6].childElementCount !== amountOfTabs);
-        console.log('test1');
         if (document.querySelectorAll('.x-box-target')[6].childElementCount !== amountOfTabs) {
-            console.log('test2');
             observer.disconnect();
             const iframe = document.querySelectorAll('iframe')[1];
             await new Promise(resolve => setTimeout(resolve, 1000));
-            console.log('test3');
             iframe.contentWindow.document.addEventListener(
                 'keyup',
                 debounce(() => {
-                    console.log('test');
                     (iframe.contentDocument || iframe.contentWindow.document).querySelector('button#ext-gen22').click();
                 }, 700)
             );
-            console.log('test4');
         }
     });
 
